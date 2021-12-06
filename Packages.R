509c192bef91185f6d164fd821104ab044aaac0e
Eric Marcon <e.marcon@free.fr>
2021-12-06 15:01:30 +0100

    Mise à jour 2021

--- Packages.R
+++ Packages.R
@@ -1,28 +1,29 @@
-# Rtools ####
-
-# Rtools : déclaration du chemin dans .Renviron
-# Nécessite de redémarrer RStudio
-# Référence : https://cran.r-project.org/bin/windows/Rtools/
-writeLines('PATH="${RTOOLS40_HOME}\\usr\\bin;${PATH}"', con = "~/.Renviron")
-# Vérification (doit renvoyer le chemin de make.exe)
-Sys.which("make")
-
-
 # Installation des packages de R ####
-
-# Installation des packages nécessaires seulement
+options(pkgType="binary")
+options(install.packages.check.source="no")
 InstallPackages <- function(Packages) {
   sapply(Packages, function(Package) 
     if (!Package %in% installed.packages()[, 1]) {install.packages(Package)})
 }
 
+# Rtools : déclaration du chemin, nécessite de redémarrer RStudio ####
+writeLines('PATH="${RTOOLS40_HOME}\\usr\\bin;${PATH}"', con = "~/.Renviron")
+# Rtools pacman : installation des packages C++
+system2("pacman", args="-Sy")                                       # Initialisation
+system2("pacman", args="-S mingw-w64-x86_64-intel-tbb --noconfirm") # Pour RcppParallel
+
+# Identification pour git
+InstallPackages("usethis")
+usethis::use_git_config(user.name = "Prenom Nom", 
+                        user.email = "prenom.nom@organisation.fr")
+
 
 # Outils de développement ####
 InstallPackages(c("devtools",   # Outils de développement. Importe remotes, etc.
                   "rcmdcheck",  # Exécution de Check par RStudio
-                  "formatR",    # Fomatage du code R (utilisé par knitr)
-                  "pkgdown",    # Documentation des packages dans /docs sur GitHub
-                  "Rdpack",     # Bilio avec roxygen
+                  "formatR",    # Formatage du code R (utilisé par knitr)
+                  "pkgdown",    # Documentation des packages sur GitHub
+                  "Rdpack",     # Biblio avec roxygen
                   "rbenchmark", # Mesure des performances
                   "roxygen2",   # Documentation automatique des packages
                   "testthat"    # Tests des packages
@@ -33,6 +34,7 @@ InstallPackages(c("devtools",   # Outils de développement. Importe remotes, etc
 InstallPackages(c("knitr",      # Tricot
                   "bookdown",   # Documents markdown complexes
                   "blogdown",   # Sites web
+                  "memoiR",     # Modèles
                   "xaringan",   # Modèle de présentation
                   "rticles"     # Modèles d'articles
                   ))
@@ -47,8 +49,3 @@ InstallPackages(c("gplots",    # Graphiques en 3D
                   "devEMF",    # Figures au format EMF
                   "ragg"       # Rendus graphiques
 ))
-
-
-# EcoFoG
-remotes::install_github("EcoFoG/EcoFoG", build_vignettes = TRUE)
-
